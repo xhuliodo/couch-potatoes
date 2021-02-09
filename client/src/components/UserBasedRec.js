ea65ea760bf92b9289b37837125f45c2b1995c14
Xhulio Doda <xhulio.doda@gov.local>
2021-02-09 16:28:11 +0100

    fixing dockerfile for new environment env

--- client/src/components/UserBasedRec.js
+++ client/src/components/UserBasedRec.js
@@ -1,40 +1,38 @@
 import "../components/MovieCard.css";
 
 import { useQuery } from "react-query";
-import {useGraphqlClient} from '../utils/useGraphqlClient'
-import request, { gql } from "graphql-request";
+import { useGraphqlClient } from "../utils/useGraphqlClient";
+import { gql } from "graphql-request";
 
 import { useMovieStore } from "../context/movies";
 import MovieCard from "./MovieCard";
 import UserFeedbackMovieCard from "./UserFeedbackMovieCard";
 import { useAuth0 } from "@auth0/auth0-react";
 
-
-
 export default function UserBasedRec() {
   const { peopleToCompare, limit, requiredMovies } = useMovieStore();
 
   const minimumRatings = requiredMovies / 1.5;
 
-  const graphqlClient = useGraphqlClient()
+  const graphqlClient = useGraphqlClient();
 
   const useUserBasedRec = (
-  userId,
-  minimumRatings,
-  peopleToCompare,
-  moviesToRecommend
-) => {
-  return useQuery(
-    [
-      "userBasedRec",
-      userId,
-      minimumRatings,
-      peopleToCompare,
-      moviesToRecommend,
-    ],
-    async () => {
-      const data = (await graphqlClient).request(
-        gql`
+    userId,
+    minimumRatings,
+    peopleToCompare,
+    moviesToRecommend
+  ) => {
+    return useQuery(
+      [
+        "userBasedRec",
+        userId,
+        minimumRatings,
+        peopleToCompare,
+        moviesToRecommend,
+      ],
+      async () => {
+        const data = (await graphqlClient).request(
+          gql`
           query{
             recommendFromOtherUsers( 
               userId: "${userId}" 
@@ -50,12 +48,12 @@ export default function UserBasedRec() {
                 } 
             }
         `
-      );
-      const { recommendFromOtherUsers } = await data;
-      return recommendFromOtherUsers;
-    }
-  );
-};
+        );
+        const { recommendFromOtherUsers } = await data;
+        return recommendFromOtherUsers;
+      }
+    );
+  };
 
   const { user } = useAuth0();
   const { isLoading, isError, data, refetch } = useUserBasedRec(
