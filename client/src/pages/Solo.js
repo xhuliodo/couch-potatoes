7d9dfe9f78ed0df4764136dbcc2180548b535e78
xhuliodo <xhuliodo@gmail.com>
2021-02-15 23:35:52 +0100

    pwa auto update works like a charm :D

--- client/src/pages/Solo.js
+++ client/src/pages/Solo.js
@@ -96,37 +96,37 @@ export const Solo = (props) => {
         <DataStateMovieCard message="Something went wrong..." />
       ) : (
         // no animation version
-        <div style={{ marginTop: "15px" }}>
-          {nav === 1 ? (
-            <UserBasedRec />
-          ) : nav === 0 ? (
-            <GenreBasedRec startedFromTheBottomNowWeHere={true} />
-          ) : nav === 2 ? (
-            <WatchlistProvider />
-          ) : null}
-        </div>
-        // <SwipeableViews
-        //   style={{ marginTop: "15px" }}
-        //   axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-        //   index={nav}
-        //   disabled={true}
-        //   onChangeIndex={handleChangeIndex}
-        //   springConfig={{
-        //     duration: "0.5s",
-        //     easeFunction: "cubic-bezier(0.42, 0, 0.58, 1)",
-        //     delay: "0s",
-        //   }}
-        // >
-        //   <Panel value={nav} index={0} dir={theme.direction}>
-        //     <GenreBasedRec startedFromTheBottomNowWeHere={true} />
-        //   </Panel>
-        //   <Panel value={nav} index={1} dir={theme.direction}>
+        // <div style={{ marginTop: "15px" }}>
+        //   {nav === "userBased" ? (
         //     <UserBasedRec />
-        //   </Panel>
-        //   <Panel value={nav} index={2} dir={theme.direction}>
+        //   ) : nav === "genreBased" ? (
+        //     <GenreBasedRec startedFromTheBottomNowWeHere={true} />
+        //   ) : (
         //     <WatchlistProvider />
-        //   </Panel>
-        // </SwipeableViews>
+        //   )}
+        // </div>
+        <SwipeableViews
+          style={{ marginTop: "15px" }}
+          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+          index={nav}
+          disabled={true}
+          onChangeIndex={handleChangeIndex}
+          springConfig={{
+            duration: "0.5s",
+            easeFunction: "cubic-bezier(0.42, 0, 0.58, 1)",
+            delay: "0s",
+          }}
+        >
+          <Panel value={nav} index={0} dir={theme.direction}>
+            <GenreBasedRec startedFromTheBottomNowWeHere={true} />
+          </Panel>
+          <Panel value={nav} index={1} dir={theme.direction}>
+            <UserBasedRec />
+          </Panel>
+          <Panel value={nav} index={2} dir={theme.direction}>
+            <WatchlistProvider />
+          </Panel>
+        </SwipeableViews>
       )}
     </Paper>
   );
