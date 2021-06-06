ed81b37326d66f822196cc9ae95ad5110c07027d
xhuliodo <xhuliodo@gmail.com>
2021-06-06 16:51:07 +0200

    done with error handling in initial_setup

--- clean-api/application/initial_setup.go
+++ clean-api/application/initial_setup.go
@@ -1,8 +1,7 @@
 package application
 
 import (
-	"errors"
-
+	"github.com/pkg/errors"
 	"github.com/xhuliodo/couch-potatoes/clean-api/domain"
 )
 
@@ -15,35 +14,46 @@ func NewSetupService(repo domain.Repository) SetupService {
 }
 
 func (ss SetupService) GetAllGenres() ([]domain.Genre, error) {
-	return ss.repo.GetAllGenres()
+	genres, err := ss.repo.GetAllGenres()
+	if err != nil {
+		errStack := errors.Wrap(err, "could not retrieve all genres")
+		return genres, errStack
+	}
+
+	return genres, nil
 }
 
 func (ss SetupService) SaveGenrePreferences(userId string, genres []string) error {
 	user, err := ss.repo.GetUserById(userId)
 	if err != nil {
-		return errors.New("a user with this identifier does not exist")
+		errStack := errors.Wrap(err, "a user with this identifier does not exist")
+		return errStack
 	}
 
 	currentGenres, err := ss.repo.GetAllGenres()
 	if err != nil {
-		return errors.New("no genres found")
+		errStack := errors.Wrap(err, "could not retrieve all genres")
+		return errStack
 	}
 
 	genresToAdd := []domain.Genre{}
 	for _, genre := range genres {
 		g, found := Find(currentGenres, genre)
 		if !found {
-			return errors.New("one of the genres id's supplied is incorrect")
+			cause := errors.New("bad_request")
+			return errors.Wrapf(cause, "genre with id %s does not exist", genre)
 		}
 		genresToAdd = append(genresToAdd, g)
 	}
 
 	if err := user.GiveGenrePreferences(genresToAdd); err != nil {
-		return err
+		cause := errors.New("bad_request")
+		return errors.Wrap(cause, err.Error())
 	}
 
 	if err := ss.repo.SaveGenrePreferences(user.Id, genresToAdd); err != nil {
-		return err
+		errStack := errors.Wrap(err, "could not save genre preferences")
+		return errStack
 	}
 
 	return nil
@@ -64,32 +74,54 @@ type SetupStep struct {
 	Message  string
 }
 
-func (ss SetupService) GetSetupStep(userId string) (SetupStep, error) {
+const (
+	leastRequiredGenrePref uint = 3
+	leastRequiredRatingNr  uint = 15
+)
+
+func (ss SetupService) GetSetupStep(userId string) (
+	sp SetupStep, err error,
+) {
 	if _, err := ss.repo.GetUserById(userId); err != nil {
-		return SetupStep{}, errors.New("a user with this identifier does not exist")
+		errStack := errors.Wrap(err, "a user with this identifier does not exist")
+		return sp, errStack
 	}
 
-	if _, err := ss.repo.GetGenrePreferences(userId); err != nil {
-		return SetupStep{
+	genrePrefCount, err := ss.repo.GetGenrePreferencesCount(userId)
+	if err != nil {
+		errStack := errors.Wrap(err, "could not get genre preferences count")
+		return sp, errStack
+	}
+
+	if genrePrefCount < leastRequiredGenrePref {
+		sp = SetupStep{
 			Step:     1,
 			Finished: false,
 			Message:  "user has yet to give genre preferences",
-		}, nil
+		}
+		return sp, nil
 	}
 
-	const leastRequiredRatingNr uint = 15
+	ratedMoviesCount, err := ss.repo.GetUserRatingsCount(userId)
+	if err != nil {
+		errStack := errors.Wrap(err, "could not get user ratings count")
+		return sp, errStack
+	}
 
-	if ratedMoviesCount, err := ss.repo.GetUserRatingsCount(userId); err != nil || !hasUserRatedLeastRequiredRatedMovies(leastRequiredRatingNr, ratedMoviesCount) {
-		return SetupStep{
+	if ratedMoviesCount < leastRequiredRatingNr {
+		sp = SetupStep{
 			Step:     2,
 			Finished: false,
 			Message:  "user has not given enough ratings to get to know him yet",
-		}, nil
+		}
+		return sp, nil
 	}
 
-	return SetupStep{Step: 3, Finished: true, Message: "user has finished the setup process"}, nil
-}
+	sp = SetupStep{
+		Step:     3,
+		Finished: true,
+		Message:  "user has finished the setup process",
+	}
 
-func hasUserRatedLeastRequiredRatedMovies(leastRequiredRatingNr, ratedMoviesCount uint) bool {
-	return ratedMoviesCount >= leastRequiredRatingNr
+	return sp, nil
 }
